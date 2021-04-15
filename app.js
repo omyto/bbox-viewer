0f130a8ba27e5deb3cee50104d1624352fb69cc9
Sua Le <sua8051@gmail.com>
2021-04-15 13:47:15 +0700

    - update export image function

--- app.js
+++ app.js
@@ -180,3 +180,127 @@ function tile2lon(x,z) {
       break;
   }
  }
+
+ 
+var createImage = function (location0, location1, zoom) {
+  let server = "http://rtile.map4d.vn/all/2d";
+  if (!global.map) {
+    return;
+  } else {
+    if (global.map.is3dMode()) {
+      server = "http://rtile.map4d.vn/all/3d";
+    }
+  }
+
+  let width = 1;
+  let height = 1;    
+  let sources = [];
+  let x0 = lon2tile(location0.lng, zoom);
+  let y0 = lat2tile(location0.lat, zoom);
+  let x1 = lon2tile(location1.lng, zoom);
+  let y1 = lat2tile(location1.lat, zoom);
+  let minx = Math.min(x0, x1);
+  let maxx = Math.max(x0, x1);
+  let miny = Math.min(y0, y1);
+  let maxy = Math.max(y0, y1);
+  width = 256 * (maxx - minx + 1);
+  height = 256 * (maxy - miny + 1);
+  for (let x = minx; x <= maxx; x += 1) {
+      for (let y = miny; y <= maxy; y += 1) {
+          let offsetX = (x - minx) * 256;
+          let offsetY = (y - miny) * 256;
+          sources.push({
+              src: `${server}/${zoom}/${x}/${y}.png`, 
+              x: offsetX,
+              y: offsetY });
+      }
+  }    
+  mergeImages(sources, {
+      width: width,
+      height: height
+    })
+      .then(b64 => downloadBase64(b64));
+}
+
+var downloadBase64 = function(data) {    
+  var a = document.createElement("a"); //Create <a>
+  a.href = data; //Image Base64 Goes here
+  a.download = "SuaDepTrai.png"; //File name Here
+  a.click();
+}
+
+var tapExport = function () {  
+  createImage(global.location1, global.location2, global.zoom);
+}
+
+
+// Defaults
+const defaultOptions = {
+format: 'image/png',
+quality: 1.0,
+width: undefined,
+height: undefined,
+Canvas: undefined,
+crossOrigin: "Anonymous"
+};
+
+// Return Promise
+const mergeImages = (sources = [], options = {}) => new Promise(resolve => {
+options = Object.assign({}, defaultOptions, options);
+
+// Setup browser/Node.js specific variables
+const canvas = options.Canvas ? new options.Canvas() : window.document.createElement('canvas');
+const Image = options.Image || window.Image;
+
+// Load sources
+const images = sources.map(source => new Promise((resolve, reject) => {
+  // Convert sources to objects
+  if (source.constructor.name !== 'Object') {
+    source = { src: source };
+  }
+
+  // Resolve source and img when loaded
+  const img = new Image();
+  img.crossOrigin = options.crossOrigin;
+  img.onerror = () => reject(new Error('Couldn\'t load image'));
+  img.onload = () => resolve(Object.assign({}, source, { img }));
+  img.src = source.src;
+}));
+
+// Get canvas context
+const ctx = canvas.getContext('2d');
+
+// When sources have loaded
+resolve(Promise.all(images)
+  .then(images => {
+    // Set canvas dimensions
+    const getSize = dim => options[dim] || Math.max(...images.map(image => image.img[dim]));
+    canvas.width = getSize('width');
+    canvas.height = getSize('height');
+
+    // Draw images to canvas
+    images.forEach(image => {
+      ctx.globalAlpha = image.opacity ? image.opacity : 1;
+      return ctx.drawImage(image.img, image.x || 0, image.y || 0);
+    });
+
+    if (options.Canvas && options.format === 'image/jpeg') {
+      // Resolve data URI for node-canvas jpeg async
+      return new Promise((resolve, reject) => {
+        canvas.toDataURL(options.format, {
+          quality: options.quality,
+          progressive: false
+        }, (err, jpeg) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(jpeg);
+        });
+      });
+    }
+
+    // Resolve all other data URIs sync
+    return canvas.toDataURL(options.format, options.quality);
+  }));
+});
